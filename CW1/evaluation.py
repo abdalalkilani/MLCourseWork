89c1c42f2d0c68af89ccd893164ac0c67fb449fe
Nicholas Mytilineos <nicholas.mytilineos@hotmail.com>
2022-10-27 12:00:42 +0100

    some eval stuff

--- CW1/evaluation.py
+++ CW1/evaluation.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 # 10-fold cross-validation on both datasets
     # - must return accuracy of tree
 
-def evaluation(test_db, trained_tree):
+def evaluate(test_db, trained_tree):
 
 
-    
+
     return accuracy
 
