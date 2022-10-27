4b0b0791978548e4435c7019166b52159bbefa9f
Nicholas Mytilineos <nicholas.mytilineos@hotmail.com>
2022-10-27 17:17:46 +0100

    very little pruning

--- CW1/pruning.py
+++ CW1/pruning.py
@@ -1,5 +1,8 @@
 import numpy as np
 from training import decision_tree_learning
 
-def pruning():
-    
+def pruning(tree):
+    if(tree['left']['depth'] and tree['right']['depth']):
+        # replace node with leaf
+    else:
+        # idk i neeed some cake
