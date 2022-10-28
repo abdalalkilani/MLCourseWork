17091d7fa59684681129d503a8ff0008e39d18f0
AbdelQader AlKilany <abdal.alkilani@gmail.com>
2022-10-28 18:02:19 +0100

    prune me zaddy

--- CW1/postpruning.py
+++ CW1/postpruning.py
@@ -4,18 +4,31 @@ from training import decision_tree_learning
 from evaluation import evaluate
 
 def postpruning(built_tree):
-    depth = decision_tree_learning
 
+    depth = decision_tree_learning
     right_node = built_tree['right']
     left_node = built_tree['left']
     left_depth = left_node['depth']
     right_depth = right_node['depth']
 
-    while(left_depth & right_depth !=0):
-        postpruning(left_depth)
-        postpruning(right_depth)
-        if left_depth & right_depth == 0:
+    old_tree = built_tree
+    initial = 0
+    current_accuracy = evaluate(built_tree)
+
+    while ((old_tree != built_tree) or (initial == 0)):
+        initial = 1
+        
+        if(left_depth != 0 & right_depth !=0):
+            postpruning(left_node)
+            postpruning(right_node)
+
+        else:
             temp_tree = built_tree.pop(left_node)
             new_tree = temp_tree.pop(right_node)
-            if evaluate(new_tree) > evaluate(built_tree):
+            new_accuracy = evaluate(new_tree)
+
+            if new_accuracy >= current_accuracy:
                 built_tree = new_tree
+                current_accuracy = new_accuracy
+
+    return built_tree
--- CW1/pruning.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from training import decision_tree_learning
-
-def pruning(tree):
-    if(tree['left']['depth'] and tree['right']['depth']):
-        # replace node with leaf
-    else:
-        # idk i neeed some cake
